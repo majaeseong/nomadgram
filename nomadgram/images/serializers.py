7d7b26d43d69214bb21042b062562d1321ddcbd3
jaeseong <cpontina@naver.com>
2018-12-23 18:55:48 +0900

    add taggitserializier

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from . import models
 from nomadgram.users import models as user_models
+from taggit_serializer.serializers import (TagListSerializerField,
+                                           TaggitSerializer)
 
 class SmallImageSerializer(serializers.ModelSerializer):
     #use for the notifications app
@@ -41,13 +43,14 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageSerializer(TaggitSerializer, serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
     #likes = LikeSerializer(many=True)
     #models의 related_name을 통해서 이름을 변경할 수 있다.
     #원래는 comment_set, like_set 
     creator = FeedUserSerializer()
+    tags = TagListSerializerField()
 
     class Meta: # Meta is extra data, 설정을 위한 클래스
         model = models.Image
@@ -59,6 +62,7 @@ class ImageSerializer(serializers.ModelSerializer):
             'comments',
             'like_count',
             'creator',
+            'tags',
             'created_at'
         )
 

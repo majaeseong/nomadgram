76f0d8922ed5f16b0aa321158b599a60906c776c
jaeseong <cpontina@naver.com>
2018-12-23 17:18:18 +0900

    setting JWT
    django에서는 쿠키로 사용자를 관리한다. 하지만 react에서는 쿠키 관리가 어렵다. 그래서 jwt를 사용

--- config/settings/base.py
+++ config/settings/base.py
@@ -253,3 +253,15 @@ SOCIALACCOUNT_ADAPTER = 'nomadgram.users.adapters.SocialAccountAdapter'
 # ------------------------------------------------------------------------------
 #taggit 대소문자 구분 X
 TAGGIT_CASE_INSENSITIVE = True
+
+#JWT -> default : 모든 request 는 승인을 받아야한다. - for protect api
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}

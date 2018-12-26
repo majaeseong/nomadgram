19d8c9f0d6ba837f468e68b4a4af47e7ac9755d1
jaeseong <cpontina@naver.com>
2018-12-26 16:53:05 +0900

    add middlewares
    thunk(대기하다가 action이 들어오면 바로 reducer로 보내줌), logger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import App from 'App';
 import {Provider} from 'react-redux';
 import store from 'redux/configureStore';
 
-console.log(store.getState());
 ReactDOM.render(
 <Provider store={store}>
     <App />
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,21 @@
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
 import users from "redux/modules/users";
+
+const env =process.env.NODE_ENV;
+            //node js 의 정보
+
+const middlewares = [thunk];
+
+if(env==='development'){
+    const {logger} = require('redux-logger');
+    middlewares.push(logger);
+}
 
 const reducer = combineReducers({
     users,
 });
 
-let store = initialState => createStore(reducer);
-
+let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+                                                             //[1,2,3]->(1,2,3) unpack
 export default store();

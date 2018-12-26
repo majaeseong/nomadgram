8d31ae830279cf5d192984ec8b19cab84e6b4554
jaeseong <cpontina@naver.com>
2018-12-26 14:51:52 +0900

    set node_path

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'index.css';
+import App from 'App';
 import {Provider} from 'react-redux';
-import store from './redux/configureStore';
+import store from 'redux/configureStore';
 
 console.log(store.getState());
 ReactDOM.render(
